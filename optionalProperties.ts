73a196db68a86dba98a51dff1a8be322c10e45fc
Akash Singh <akashsinghaa008@gmail.com>
2025-09-02 23:43:52 +0530

    add TypeScript examples for optional properties and student data display

---
+++ optionalProperties.ts
@@ -0,0 +1,52 @@
+type Marks = {
+    maths : number;
+    english : number;
+    hindi : number;
+    physical? : number; //optional 
+}
+
+type student = {
+    name : string;
+    enroll : number;
+    age : number;
+    class : string; 
+}
+
+const totalMarks = (marks: Marks): number => {
+    return marks.english + marks.hindi + marks.maths + (marks.physical ?? 0);
+};
+
+function StudentDataDisplay(studentData : student,data : Marks){
+    console.log(`
+        ======================
+        |   Student Schema   |
+        ======================
+        |Name      : ${studentData.name}   |
+        |Age       : ${studentData.age}      |
+        |Roll NO.  : ${studentData.enroll}|
+        |Class     : ${studentData.class}    |
+        ======================
+
+        ======================
+        |      Marksheet     |
+        ======================
+        | Subject  |  Marks  |
+        ======================       
+        | Maths    |    ${data.maths}   |
+        | English  |    ${data.english}   |  
+        | Hindi    |    ${data.hindi}   |
+        | Physical |    ${data.physical??0}   |   
+        ======================
+        | Total    |    ${totalMarks(data)}  |
+        ======================
+        `)
+};
+
+StudentDataDisplay(
+    {name:"Akash",enroll: 8811604424,age : 23,class:"10th"},
+    {maths:92,english:88,hindi:39,physical : 80}
+);
+StudentDataDisplay(
+    {name:"Tanisha",enroll: 1111604424,age : 23,class:"12th"},
+    {maths:92,english:88,hindi:39}
+);
